4e48218d9e75c6677b3b291c3f24ab6b553069f5
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-01-13 23:03:25 +0200

    Create comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_13_205804) do
+ActiveRecord::Schema.define(version: 2022_01_13_210229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
