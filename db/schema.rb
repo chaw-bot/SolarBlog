6fcdb3cb07ca5bf63ace1c29c27b6a8251f301ef
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-01-13 22:38:24 +0200

    Add to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_13_095345) do
+ActiveRecord::Schema.define(version: 2022_01_13_203525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "photo"
+    t.text "bio"
+    t.integer "posts_counter"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
