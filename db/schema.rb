22fabf296c8c9aca2e250762a1ead4ea6f8ca1e5
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-01-13 22:40:59 +0200

    Add posts to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_13_203525) do
+ActiveRecord::Schema.define(version: 2022_01_13_203908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.integer "likes_counters"
+    t.integer "comments_counter"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
