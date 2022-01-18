0e07b546d3b7ff3fe1c1e67d370242d86e08fe3c
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-01-18 12:31:59 +0200

    Add methods to increase posts and display recent comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ApplicationRecord
     belongs_to: users
     has_many: likes, :comments
+
+    def update_user_post_counter
+        users.posts_counter +=1
+    end
+
+    def return_most_recent_comments
+        comments.last(5)
+    end
 end
