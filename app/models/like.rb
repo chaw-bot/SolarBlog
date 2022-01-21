6a97dda4cfe8d5bcf7dd4ea17e57ba565a54a53a
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-01-21 18:40:49 +0200

    Refactor model code

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,11 +1,12 @@
 class Like < ApplicationRecord
-  belongs_to :post, :user
+  belongs_to :post
+  belongs_to :user
 
   after_save :update_likes_counter
 
   private
 
   def update_likes_counter
-    post.likes_counter += 1
+    post.likes_counters += 1
   end
 end
