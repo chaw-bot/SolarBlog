5d0af36adc8b8b4381f33d4fe05cff6c5ea80de3
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-01-22 00:01:15 +0200

    Add validations in users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  validates :name, presence: true
+  validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def return_last_three_posts
     posts.last(3)
   end
