534258b1c7ce79e9d166c366493c79453ff01273
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-01-27 02:23:31 +0200

    Add role column

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root "users#index"
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
       post "new_comment", to: "comments#create", on: :member
       post "like_post", to: "likes#create", on: :member
     end
--- db/migrate/20220126201228_add_default_value_to_role.rb
+++ db/migrate/20220126201228_add_default_value_to_role.rb
@@ -1,5 +1,9 @@
 class AddDefaultValueToRole < ActiveRecord::Migration[7.0]
-  def change
+  def up
+    change_column :users, :role, :string
+  end
+
+  def down
     change_column :users, :role, :string, default: "admin"
   end
 end
