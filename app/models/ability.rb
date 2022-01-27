38da083a5a6e0c850c5e9b317df274765b10a4aa
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-01-27 02:25:35 +0200

    Set up ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,17 +1,18 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
-    def initialize(user)
-      can :read, Post  # start by defining rules for all users, also not logged ones
-      return unless user.present?
-      can :manage, Post, user_id: user.id # if the user is logged in can manage it's own posts
-      can :create, Comment # logged in users can also create comments
-      return unless user.manager? # if the user is a manager we give additional permissions
-      can :manage, Comment # like managing all comments in the website
-      return unless user.admin?
-      can :manage, :all # finally we give all remaining permissions only to the admins
-    end
+  def initialize(user)
+    can :read, Post
+    can :read, Comment
+    can :read, User
+    can :read, Like
+    return unless user.present?
+
+    can :manage, Post, user_id: user.id
+    can :manage, Comment, user_id: user.id
+    can :manage, Like, user_id: user.id
+    return unless user.admin?
+
+    can :manage, :all
     # The first argument to `can` is the action you are giving the user
     # permission to do.
     # If you pass :manage it will apply to every action. Other common actions
