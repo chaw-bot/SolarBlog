4f0cdfe4dd45a244a01b88f30c0ce2aca667930f
ntzwilly <ntzwilly@gmail.com>
2022-01-28 12:52:14 +0200

    Create tests and fix linter errors

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'capybara/rspec'
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -32,7 +33,6 @@ begin
   end
 
   Capybara.javascript_driver = :selenium_chrome
-
 rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
@@ -91,5 +91,5 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 
-  Capybara.default_driver = :selenium_chrome
+  # Capybara.default_driver = :selenium_chrome
 end
