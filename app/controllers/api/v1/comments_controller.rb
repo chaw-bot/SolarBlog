11747ba10a78e7e429ad54a95abb524d5998a0c3
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-02-04 13:31:30 +0200

    Add command to authenticate user before actions

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -2,15 +2,36 @@ module Api
   module V1
     class CommentsController < ApplicationController
       before_action :show_post
+      before_action :authenticate_user!
 
       def index
         @comments = show_post.comments.includes(:user)
       end
 
+      def create
+        comment = Comment.new(comments_params)
+        comment.post_id = params[:post_id]
+        comment.user = current_user
+
+        if comment.save
+          render json: { status: 201, message: 'Comment created successfully!', content: { comment: comment } }
+        else
+          render json: comment.errors, status: :bad_request, message: 'Operation failed'
+        end
+      end
+
       private
 
+      def find_user
+        User.find(params[:user_id])
+      end
+
       def show_post
-        Post.find(params[:post_id])
+        find_user.posts.find(params[:post_id])
+      end
+
+      def comments_params
+        params.require(:comment).permit(:text, :user_id, :post_id)
       end
     end
   end
