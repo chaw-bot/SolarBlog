89d7146bf6b2de1e074b33d37b64a77c4cd2e2d3
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-02-04 14:13:14 +0200

    Refactor permissions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,34 +1,15 @@
 class Ability
   include CanCan::Ability
   def initialize(user)
-    can :read, Post
-    can :read, Comment
-    can :read, User
-    can :read, Like
     return unless user.present?
-
-    can :manage, Post, user_id: user.id
-    can :manage, Comment, user_id: user.id
-    can :manage, Like, user_id: user.id
-    return unless user.admin?
-
-    can :manage, :all
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+    
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+      can :create, [Post, Comment, Like]
+      can :update, [Post, Comment, Like], user_id: user.id
+      can :destroy, [Post, Comment], user_id: user.id
+    end    
   end
 end
