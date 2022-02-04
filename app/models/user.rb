77d94c51346884e8e3eca2171e4c9c6c26820d26
chaw <77142475+chaw-bot@users.noreply.github.com>
2022-02-04 14:16:13 +0200

    Add jwt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
   has_many :posts
   has_many :likes
   has_many :comments
